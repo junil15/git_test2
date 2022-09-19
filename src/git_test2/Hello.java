fad5042192501f7a095bde8b2638f76aeb416737
junil15 <junilg15@gmail.com>
2022-09-19 15:29:38 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,10 +1,17 @@
 package git_test2;
 
 public class Hello {
+
+	String message;
+	
 	public void say() {
 
 		String message = "안녕하세요";
 
 		System.out.println(message);
 	}
+
+	public Hello() { this("안녕하세요"); }
+
+	public Hello(String message) { this.message = message; }
 }
